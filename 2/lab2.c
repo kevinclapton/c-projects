61417588f1f46aa646a1f53b8b433f8369d0934f
Semyon Varlamov <sem.varlamow@mail.ru>
2019-10-14 02:16:46 +0700

    Tests and questions

--- 2/lab2.c
+++ 2/lab2.c
@@ -1,5 +1,16 @@
-#include <stdlib.h> //Добавление стандартной библиотеки ввода/вывода
+#include <stdlib.h>
+#include <stdio.h>
+#define MAX_LEN 1024
 
 int main(int argc, char **argv) {
-	return 0; //Тестовый комментарий(!)
+	/*int s=1;
+	printf("Введите количество строк: ");
+	scanf("%d", &s);
+	const int S = s;*/
+	char buf[MAX_LEN];
+	printf("Строка №1: ");
+	fgets(buf, MAX_LEN, stdin);
+	fflush(stdin);
+	printf("Вот строка: %s\n", buf);
+	return 0;
 }
