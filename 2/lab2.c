2be82a5261db8290fe79665d2b71c731563c3b31
Semyon Varlamov <sem.varlamow@mail.ru>
2019-10-17 21:21:17 +0700

    ((((((((

--- 2/lab2.c
+++ 2/lab2.c
@@ -1,35 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-#define MAX_LEN 1024
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 128
 
-void pr(char string[]);
-char* inp_str(char* string; int maxlen);
-
-int main(int argc, char **argv) {
-	int s;
-	printf("Введите количесто строк: ");
-	scanf("%d%*c", &s);
-	char** strs = NULL;
-	strs = (char**)malloc(sizeof(char)*s);
-	char buf[MAX_LEN];
-	for (unsigned i=0; i<s; i++) {
-		printf("Строка %d:", i+1);
-		fgets(buf, MAX_LEN, stdin);
+void inp_str(char*** string, unsigned s) {
+	if((*string = (char**)malloc(s*sizeof(char*))) == 0)
+		exit(1);
+	char buf[MAXLEN];
+	for(int i=0; i<s; i++) {
+		printf("Введите строку №%d: ", i+1);
+		fgets(buf, MAXLEN, stdin);
 		fflush(stdin);
-		strs[i] = malloc(sizeof(char)*strlen(buf));
-		strcpy(strs[i], buf);
+		if ((*string[i] = (char*)malloc(strlen(buf)*sizeof(char))) == 0)
+			exit(2);
+		strcpy(*string[i],buf);
 	}
-	for (unsigned i=0; i<s; i++)
-		pr(strs[i]);
-	free(strs);
+}
+
+void out_strs(char*** string, unsigned s) {
+	for(int i=0; i<s; i++)
+		printf("Строка %d: %s\n", i+1, *string[i]);
+}
+
+void free_strs(char*** string, unsigned s) {
+	for(int i=0; i<s; i++)
+		free(*string[i]);
+	free(*string);
+}
+
+int main(int argc, char** argv) {
+	unsigned s;
+	printf("Введите количество строк: ");
+	scanf("%u%*c", &s);
+	char** string = NULL;
+	inp_str(&string, s);
+	out_strs(&string, s);
+	free_strs(&string, s);
 	return 0;
-}
-
-void pr(char string[]) {
-	printf("%s", string);
-}
-
-char* inp_str(char* string; int maxlen) {
-	
 }
